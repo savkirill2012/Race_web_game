ebd44fac6e8a61338fc98a1b9aa152762f39d779
savkirill2012 <savkirill2012@gmail.com>
2024-06-21 05:06:55 +0700

    add gunicorn and fix CORS on fastapi serv

--- src/server/main.py
+++ src/server/main.py
@@ -6,12 +6,16 @@ import asyncio
 
 app = FastAPI()
 
+origins = ["http://localhost:5173",]
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 
