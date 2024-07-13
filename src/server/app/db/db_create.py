169f384e02d31299d5cef82a265a851c32a680f4
Sharika Jacob <sharikaj@mi-i6.msk.21-school.ru>
2024-07-13 19:52:39 +0300

    add create simple table in psql + get data from it

--- src/server/app/db/db_create.py
+++ src/server/app/db/db_create.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship, Session
 
-engine = create_engine('postgresql+psycopg2://test:test@localhost:5432/test')
+engine = create_engine('postgresql+psycopg2://test:test@localhost:5432/test', echo=True)
 
 try:
     engine.connect()
@@ -10,16 +10,35 @@ except:
     print('Error ocure while trying to connect to db')
 
 
-# Base = declarative_base()
+Base = declarative_base()
 
-# class Users(Base):
-#     __tablename__ = 'users'
+class Users(Base):
+    __tablename__ = 'users'
 
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String(255), unique=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), unique=True, nullable=False)
+    # leaderboard = relationship('LeaderBoard', uselist=False)
+
+    def __repr__(self) -> str:
+        return f'{self.id} {self.username}'
 
 
 # class LeaderBoard(Base):
 #     __tablename__ = 'leaderboard'
 
-#     id = Column()
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String(255), ForeignKey('leaderboard.username'), unique=True, nullable=False)
+#     score = Column(Integer)
+
+#     def __repr__(self) -> str:
+#         return f'{self.id} {self.username} {self.score}'
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    with Session(autoflush=False, bind=engine) as db:
+        # new_user = Users(username='Test5')
+        # db.add(new_user)
+        # db.commit()
+        res = db.query(Users).all()
+        print(res)
+
